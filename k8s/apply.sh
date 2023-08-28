f1009a2f40b648b4305ffc4c6e2bbc5657be3a6d
Shubham Varshney <16763337+shubhamv108@users.noreply.github.com>
2023-08-28 13:03:26 +0530

    uf

---
+++ k8s/apply.sh
@@ -0,0 +1,3 @@
+kubectl apply -f mysql.yaml
+kubectl apply -f kafka.yaml
+kubectl apply -f app.yaml
---
+++ k8s/base-setup.sh
@@ -0,0 +1,2 @@
+kubectl apply -f ./namespace-kafka.yaml
+kubectl apply -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
