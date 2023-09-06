5dcec8a40ced017fd1ee95562c559d8834109d04
Shubham Varshney <16763337+shubhamv108@users.noreply.github.com>
2023-09-06 19:08:00 +0530

    uf

--- scripts/k8s/apply.sh
+++ scripts/k8s/apply.sh
@@ -1,5 +1,7 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/baremetal/deploy.yaml
 kubectl apply -f k8s/mysql.yaml
 kubectl apply -f k8s/kafka.yaml
 kubectl apply -f k8s/kafdrop.yaml
@@ -7,5 +9,6 @@ kubectl create -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
 kubectl apply -f k8s/es.yaml
 kubectl create namespace prometheus
-helm install prometheus bitnami/kube-prometheus
+helm install prometheus bitnami/kube-prometheus -n prometheus
 kubectl apply -f k8s/app.yaml
+kubectl apply -f k8s/grafana.yaml
